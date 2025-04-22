772cb1c9b6be26c665cd6a43c0d66439b261a503
Jasmine Morrell <jasminemorrell23@gmail.com>
2025-04-22 14:39:02 +0700

    Edit project

--- app.py
+++ app.py
@@ -43,6 +43,22 @@ def learn():
 def lesson1():
     return render_template('lesson1.html')
 
+@app.route('/lesson2')
+def lesson2():
+    return render_template('lesson2.html')
+
+@app.route('/lesson3')
+def lesson3():
+    return render_template('lesson3.html')
+
+@app.route('/lesson4')
+def lesson4():
+    return render_template('lesson4.html')
+
+@app.route('/lesson5')
+def lesson5():
+    return render_template('lesson5.html')
+
 @app.route('/get_native_audio')
 def get_native_audio():
     text = request.args.get('text', '')
